d44eb50d1138afc4281b6ddb67eaefb7d3ec42b1
Naresh Jonnala <ff.jonnala@gmail.com>
2016-05-11 14:15:57 +0530

    every page has to root index page. inner page hit comes from client

--- aweb/website/urls.py
+++ aweb/website/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls import include, url, patterns
 urlpatterns = patterns(
         "website.views",
         url(r'^$', "index", name="index"),
-        url(r'^home/$', "home", name="index"),
-        url(r'^courses/$', "courses", name="index"),
-        url(r'^students/$', "students", name="index"),
+        url(r'^home/$', "index", name="index"),
+        url(r'^courses/$', "index", name="index"),
+        url(r'^students/$', "index", name="index"),
     )
